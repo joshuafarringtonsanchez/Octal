e68f28b32f71c1c4b3dee4d88a3aeb5ebbaea689
joshuafarringtonsanchez <72886371+joshuafarringtonsanchez@users.noreply.github.com>
2021-10-08 02:04:35 -0400

    Add files via upload

---
+++ burger-button-scroll-forbid.js
@@ -0,0 +1,12 @@
+let headerInput = document.querySelector("input#burger-button");
+headerInput.addEventListener("click",scrollForbid);
+
+function scrollForbid(){
+    if(this.checked){
+        document.documentElement.style.overflow="hidden";
+        window.scrollTo(0,0);
+    }
+    if(!this.checked){
+        document.documentElement.style.overflow="visible";
+    }
+}
